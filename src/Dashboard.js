b6da5c48607af5a68c4be14ba440cf780405cfa9
irenecoder <wairene60@gmail.com>
2022-11-12 18:10:22 +0300

    dropdown

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -6,33 +6,22 @@ function Dashboard(props) {
     //get token
     //curl -X POST -d "grant_type=password&username=<user_email>&password=<user_password>&scope=read" -u"5O1KlpwBb96ANWe27ZQOpbWSF4DZDm4sOytwdzGv:PqV0dHbkjXAtJYhY9UOCgRVi5BzLhiDxGU91kbt5EoayQ5SYOoJBYRYAYlJl2RetUeDMpSvhe9DaQr0HKHan0B9ptVyoLvOqpekiOmEqUJ6HZKuIoma0pvqkkKDU9GPv" http://api.kmhfl.health.go.ke/o/token/
 
-    // const getToken=async()=>{
-    //     const token='';
-    //     try{
-    //         const url="";
-    //         fetch(url,{
-    //             method:"POST",
-
-    //         },
-    //         headers:{
-    //             "authorization":`Bearer ${token}`
-    //         }
-    //         )
-    //     } catch (err){
-    //         console.log(err.message);
-    //     }
-    // }
      //get api data
     const getApiData=async()=>{
         try {
+            const token="Qmm5CzcQX2OKrzYfRFPVpa6eKKi7DM";
             const url1='http://api.kmhfltest.health.go.ke/api/facilities/facilities/?format=json';
             const url="http://localhost:5000/county";
-            const response=await fetch(url,{
-                method:'Get'
+            const response=await fetch(url1,{
+                method:'Get',
+                headers:{
+                    authorization:`bearer ${token}`,
+                }
             }
         )
             const parseRes=await response.json();
-            setData(parseRes);
+            setData(parseRes.results);
+            console.log(parseRes.results)
         } catch (error) {
             console.log("Error: ",error.message)
         }
@@ -43,6 +32,7 @@ function Dashboard(props) {
     return (
     <>
         <Navbar/>
+
         <div className='body' style={{marginTop:"20px", marginLeft:"20px"}}>
             
             <div style={{display:"flex"}}><Link to="/" style={{color:'green'}}>Home</Link> <span class="material-symbols-outlined">chevron_right</span> <p>Dashboard</p></div>
@@ -53,23 +43,29 @@ function Dashboard(props) {
             <div style={{display:"inline-block",flexGrow:"1",marginRight:"10px"}}>
             <form class="d-flex" style={{float:"right",width:"500px", height:"50px"}}>
                 <p style={{fontWeight:"large",marginTop:"10px",marginLeft:"10px"}}>County:</p><br/>
-
-                <ul class="navbar-nav">
+                {/* <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                    {Data&&Data.map((data)=>(
+                        <div key={data.id}>
+                            <option selected>County</option>
+                         <option value="1">{data.county}</option>
+                        </div>
+                    ))}
+                </select> */}
+                <ul class="navbar-nav" style={{marginLeft:"10px"}}>
                     <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         County
                     </a>
                     <ul class="dropdown-menu">
                     {Data&&Data.map((data)=>(
-                        <div key={data.key}>
+                        <div key={data.id}>
                             <li>
                             <a class="dropdown-item" href="#">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                {data.name}
-                                </a>
-                                <ul class="dropdown-menu">
-                                <a class="dropdown-item" href="#">hy</a>
-                                </ul>
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">{data.county}</a>
+                                    <ul class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">hy</a>
+                                        <a class="dropdown-item" href="#">hy</a>
+                                    </ul>
                             </a>
                             </li>
                         </div>
